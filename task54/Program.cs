88961dc2c3b5253aec7f45c5502c3cdc4657ce5b
AlinaBarkovskaa <a.barkovskaa@icloud.com>
2022-12-16 00:35:26 +0400

    Added task

---
+++ task54/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.WriteLine("введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+
+void InputArray(int[,] matrix)
+ {
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+       for(int j = 0; j < matrix.GetLength(1); j++)
+       {
+          matrix[i, j] = new Random().Next(0, 11);
+           Console.Write($"{matrix[i, j]} \t");
+       }
+        Console.WriteLine();
+    }
+ } 
+ void sortDescending(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+      {
+        if (matrix[i, k] < matrix[i, k + 1])
+        {
+          int temp = matrix[i, k + 1];
+          matrix[i, k + 1] = matrix[i, k];
+          matrix[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+void Print(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+  }
+}
+ Console.Clear();
+ InputArray(matrix);
+ sortDescending(matrix);
+ Console.WriteLine();
+ Print(matrix);
