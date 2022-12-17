0c74e9c0edc10f5dbd81cba63c875dca4997be08
AlinaBarkovskaa <a.barkovskaa@icloud.com>
2022-12-17 13:14:51 +0400

    Added task

--- task60/Program.cs
+++ task60/Program.cs
@@ -1,31 +1,34 @@
-﻿int[,,] InputMatrix(int[,,] ma)
+﻿ void InputArray(int[,,] matrix)
 {
-    int n = 2;
-    int[,,] matrix = new int[n, n, n];
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
             for (int k = 0; k < matrix.GetLength(2); k++)
             {
-                matrix[i, j, k] = new Random().Next(10, 100);
-                Console.Write(matrix[i, j, k] + "\t");
+                matrix[i, j, k] = new Random().Next(1, 21);
             }
-            Console.WriteLine();
         }
-        Console.WriteLine();
     }
-    return matrix;
 }
-void MatrixOutputAlongWithIndices(int[,,] matrix, int n)
+static void PrintArray(int[,,] matrix)
 {
-    int index = 0;
-    while (matrix.Length != n - 1)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        index++;
+        System.Console.WriteLine((i+1));
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write(matrix[i, j, k] + "("+i+","+j+","+k+")  ");
+            }
+            System.Console.WriteLine();
+        }
+        System.Console.WriteLine();
     }
-    Console.Write($"matrix[i{index}, j{index}, k{index}] \t");
 }
 Console.Clear();
-int[, ,] array = InputMatrix(array);
-MatrixOutputAlongWithIndices(matrix, n);
+int n = 2;
+int[,,] array = new int[n, n, n];
+InputArray(array);
+PrintArray(array);
