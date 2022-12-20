a501fa35342f2c74a46c7f5bd493edb8ab79dda3
AlinaBarkovskaa <a.barkovskaa@icloud.com>
2022-12-20 21:53:01 +0400

    apruved task

--- task62/Program.cs
+++ task62/Program.cs
@@ -1,41 +1,46 @@
-﻿int[,] fillArraySnake(int n)
+﻿void RealeseSpiralArray(int[,] array)
 {
-    int[,] matrix = new int[n, n];
-    int value = 1;
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        if(i % 2 == 0)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-          for (int j = 0; j < matrix.GetLength(1); j++) 
-          {
-              matrix[i, j] = value;
-              value++;
-          }
-        } else
-        {
-           for(int j = n -1; j >= 0; j--)
-           {
-              matrix[i, j] = value;
-              value++;
-           }
+            if (array[i, j] / 10 <= 0)
+                Console.Write($" {array[i, j]} ");
+            else Console.Write($"{array[i, j]} ");
         }
+        Console.WriteLine();
     }
-    return matrix;
 }
-void Print(int[,] matrix)
+void SpiralArray(int[,] array)
 {
-   for(int i = 0; i < matrix.GetLength(0); i++)
-   {
-     for(int j = 0; j < matrix.GetLength(1); j++)
-     {
-        Console.Write(matrix[i, j] + "\t");
-     }
-     Console.WriteLine();
-   }
-}
+    int temp = 1;
+    int i = 0;
+    int j = 0;
 
-Console.Clear();
-int n = 4;
-int[,] array = new int[n, n];
-int[,] matrix = fillArraySnake(n);
-Print(matrix);
+    while (temp <= array.GetLength(0) * array.GetLength(1))
+    {
+        array[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+        {
+          j++;
+        }   
+        else if (i < j && i + j >= array.GetLength(0) - 1)
+        {
+           i++;
+        }         
+        else if (i >= j && i + j > array.GetLength(1) - 1)
+        {
+           j--;
+        }           
+        else
+        {
+           i--;
+        }     
+    }
+
+    RealeseSpiralArray(array);
+}
+int size = 4;
+int[,] array = new int[size, size];
+SpiralArray(array);
